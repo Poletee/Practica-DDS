062ee000c8e8f807883d360ace3be20f173813e1
GG <guillemgordillo@gmail.com>
2018-06-03 17:08:49 +0200

    minor changes

--- data_sets.R
+++ data_sets.R
@@ -1,10 +1,10 @@
-library(stringr)
+library(stringr) #++dplyr
 
-setwd("C:\Users\Goordi\Desktop\master\datadriven") # WORKING DIRECTORY
+setwd("C:\\Users\\Goordi\\Desktop\\master\\datadriven\\blocklist-ipsets-master") # WORKING DIRECTORY
 src.files <- list.files() # LLEGIR FITXERS DEL WORKING DIRECTORY
 IPS <- NULL
 for (file_name in src.files){
-  tmp <- read.table(file = paste("C:\Users\Goordi\Desktop\master\datadriven",
+  tmp <- read.table(file = paste("C:\\Users\\Goordi\\Desktop\\master\\datadriven\\blocklist-ipsets-master",
                                  file_name, sep = "\\"), skipNul = T, col.names = c("ip"), na.strings = "NULL", stringsAsFactors = F)
   
   file_info <- read.table(file=file_name, comment.char="/", sep = "\t", stringsAsFactors = F,nrows=50 )
