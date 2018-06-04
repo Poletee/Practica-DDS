326f538d1ce4e1997c9f30e5892853c920e6a90c
Poletee <pol94bel@hotmail.com>
2018-06-04 16:28:22 +0200

    Added methods

--- data_sets.R
+++ data_sets.R
@@ -44,3 +44,41 @@ for (file_name in src.files){
   tmp2$country <- rep(x = country, nrow(tmp2))
   countries <- rbind(countries,tmp2)
 }
+
+
+# Mostra les IPs i els cops que han estat atacats
+list.ip.count <- function(df, ip){
+  mydf <- (as.data.frame(table(df[1])))
+  mytable <- table(df[1])
+  #t <- table(df[1])
+  #print(which(mydf == ip))
+  #print(mydf[957])
+  print(dplyr::filter(str_detect(mytable, ip)))
+  print(mytable[mytable[,1]==ip,1])
+  
+  dplyr::filter(file_info, stringr::str_detect(V1,"--"))
+}
+
+IPsRecount <- list.ip.count(IPS, '5.188.86.174')
+
+# Mostra les diferents categories d'atac
+list.category <- function(df){
+  print (dplyr::distinct(df[2]))
+}
+
+# Mostra les diferents categories d'atac i el recompte de cada un
+list.category.count <- function(df){
+  print(as.data.frame(table(df[2])))
+}
+
+# Mostra tots els diferents països que tenen IPs que han sigut atacades
+list.country <- function(df){
+  print (dplyr::distinct(df[3]))
+}
+
+# Mostra una taula amb els diferents països i el recompte d'atacs rebuts
+list.country.count <- function(df){
+  print (table(df[3]))
+}
+
+list.category.count(IPS)
